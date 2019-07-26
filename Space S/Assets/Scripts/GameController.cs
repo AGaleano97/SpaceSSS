3b060b357daedf117b5e21d38b736053b503bbef
AGaleano97 <52254706+AGaleano97@users.noreply.github.com>
2019-07-26 18:30:33 -0400

    Added new Aesteroids

--- Space S/Assets/Scripts/GameController.cs
+++ Space S/Assets/Scripts/GameController.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public GameObject[] hazards;
 
     public Vector3 spawnValues;
 
@@ -16,11 +17,45 @@ public class GameController : MonoBehaviour
 
     public float waveWait;
 
+    public Text ScoreText;
+
+    public Text restartText;
+
+    public Text gameOverText;
+
+    public Text winText;
+
+    public Text creditText;
+
+    private bool gameOver;
+
+    private bool restart;
+
+    private int score;
+
     void Start ()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        winText.text = "";
+        creditText.text = "";
+        score = 0;
+        UpdateScore ();
         StartCoroutine (SpawnWaves ());
     }
 
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+    }
     IEnumerator SpawnWaves ()
     {
         yield return new WaitForSeconds (startWait);
@@ -28,12 +63,44 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate (hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds (spawnWait);
             }
             yield return new WaitForSeconds (waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
         }
+    }
+
+    public void Addscore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore ();
+    }
+
+    void UpdateScore ()
+    {
+        ScoreText.text = "Score: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You Win!";
+            creditText.text = "Game created by: Alejandro Galeano";
+            gameOver = true;
+            restart = true;
+        }
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        creditText.text = "Game created by: Alejandro Galeano";
+        gameOver = true;
     }
 }
